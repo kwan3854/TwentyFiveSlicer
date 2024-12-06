d28ac2b4c35bfbfdd91206ea92caa1e26d2bae21
KwanJoong Lee <leekwanjung@gmail.com>
2024-12-06 16:02:08 +0900

    Add context menu

---
+++ Editor/TwentyFiveSliceImageMenu.cs
@@ -0,0 +1,40 @@
+﻿using TwentyFiveSlicer.Runtime;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TwentyFiveSlicer.TFSEditor.Editor
+{
+    public static class TwentyFiveSliceImageMenu
+    {
+        [MenuItem("GameObject/UI/Image - 25 Slice", false, 2000)]
+        public static void CreateTwentyFiveSliceImage()
+        {
+            // Find or create a Canvas
+            Canvas canvas = Object.FindObjectOfType<Canvas>();
+            if (canvas == null)
+            {
+                var canvasGo = new GameObject("Canvas");
+                canvas = canvasGo.AddComponent<Canvas>();
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                canvasGo.AddComponent<CanvasScaler>();
+                canvasGo.AddComponent<GraphicRaycaster>();
+
+                Undo.RegisterCreatedObjectUndo(canvasGo, "Create Canvas");
+            }
+
+            // Create the TwentyFiveSliceImage GameObject
+            var imageGo = new GameObject("TwentyFiveSliceImage", typeof(RectTransform));
+            Undo.RegisterCreatedObjectUndo(imageGo, "Create TwentyFiveSliceImage");
+
+            imageGo.transform.SetParent(canvas.transform, false);
+
+            var image = imageGo.AddComponent<TwentyFiveSliceImage>();
+
+            var rt = imageGo.GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(100, 100);
+
+            Selection.activeGameObject = imageGo;
+        }
+    }
+}
